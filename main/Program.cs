df1ff8b6e2c0e0a1ad80c18d3954452e700d6bf9
HD-29139 <santanajefferson594@gmail.com>
2025-05-04 23:20:18 -0300

    things

--- main/Program.cs
+++ main/Program.cs
@@ -1,4 +1,5 @@
-﻿using control_public_events.main;
+﻿using System.Runtime.Intrinsics.Arm;
+using control_public_events.main;
 
 class Program
 {
@@ -6,9 +7,9 @@ class Program
 
     static void Main(string[] args)
     {
+        Console.Clear();
         while (true)
         {
-
             Console.WriteLine("\t");
             Console.WriteLine("1. Register Event");
             Console.WriteLine("2. Register Person in Event");
@@ -29,6 +30,12 @@ class Program
                     ListEvents();
                     break;
                 case "4":
+                    Console.Clear();
+                    Console.WriteLine("Bye");
+                    Thread.Sleep(500);
+                    Console.Clear();
+                    Console.WriteLine("By. HD-29139");
+                    Thread.Sleep(100);
                     return;
 
                 default:
@@ -40,6 +47,8 @@ class Program
 
         static void EventRegister()
         {
+            Console.Beep();
+            Console.Clear();
             Event newEvent = new Event();
 
             Console.WriteLine("Event Name: ");
@@ -60,15 +69,20 @@ class Program
             {
                 newEvent.Value = 0;
             }
-
+            Console.Clear();
             events.Add(newEvent);
-            Console.WriteLine("Event registered successfully!");
-
-
+            for (int i = 0; i < 22; i++)
+            {
+                Console.Clear();
+                Console.Write("Registering" + new string('.', i % 22));
+                Thread.Sleep(50);
+            }
+            Console.WriteLine($"Event {newEvent.Name} registered successfully! {DateTime.Now}");
         }
 
         static void PersonRegister()
         {
+            Console.Clear();
             if (events.Count == 0)
             {
                 Console.WriteLine("No events registered.");
@@ -78,20 +92,19 @@ class Program
             Console.WriteLine("Available events:");
             for (int i = 0; i < events.Count; i++)
             {
-                Console.WriteLine($"{i + 1}. {events[i].Name}");
-                Console.WriteLine($"{events[i].Participants.Count}|20 Peoples");
+                Console.WriteLine($"{i + 1}. {events[i].Name}, ({events[i].Participants.Count}/20) Peoples");
             }
 
             Console.WriteLine("Choose the event by number: ");
             int index = Convert.ToInt32(Console.ReadLine()) - 1;
             if (index < 0 || index >= events.Count)
             {
-                Console.WriteLine($"Invalid Event{index}");
+                Console.WriteLine($"Invalid Event \"{index + 1}\"");
                 return;
             }
 
             Person newPerson = new Person();
-
+            Console.Clear();
             Console.WriteLine("Participant Name: ");
             newPerson.Name = Console.ReadLine();
 
@@ -100,23 +113,43 @@ class Program
 
             Console.WriteLine("Participant Age: ");
             newPerson.Age = Convert.ToInt32(Console.ReadLine());
-
+            Thread.Sleep(60);
             if (events[index].AddPerson(newPerson))
             {
-                Console.WriteLine($"Person registered for the event{events[index].Name}");
+                for (int i = 0; i < 15; i++)
+                {
+                    Console.Clear();
+                    Console.Write($"\nAdding {newPerson.Name} in {events[index].Name}" + new string('.', i % 15));
+                    Thread.Sleep(60);
+                }
+                Console.WriteLine($"\n{newPerson.Name} registered for the event {events[index].Name}");
             }
+
         }
 
         static void ListEvents()
         {
+            Console.Clear();
+            if (events.Count == 0)
+            {
+                Console.WriteLine("No events registered.");
+            }
             foreach (var newEvent in events)
             {
-                Console.WriteLine($"Event Name: {newEvent.Name} | Event Local: {newEvent.Local} | Event Value: R${newEvent.Value}");
-                Console.WriteLine($"Participants ({newEvent.Participants.Count}20):");
-                foreach (var person in newEvent.Participants)
+                Console.WriteLine($"\nEvent Name: {newEvent.Name} | Event Local: {newEvent.Local} | Event Value: ${newEvent.Value}");
+                if (newEvent.Participants.Count == 0)
                 {
-                    Console.WriteLine($"- {person.Name} {person.Age} years Address: {person.Address}");
+                    Console.WriteLine($"Participants ({newEvent.Participants.Count}/20)");
                 }
+                else
+                {
+                    Console.WriteLine($"Participants ({newEvent.Participants.Count}/20):");
+                    foreach (var person in newEvent.Participants)
+                    {
+                        Console.WriteLine($"- {person.Name} {person.Age} years Address: {person.Address}");
+                    }
+                }
+
             }
         }
 
