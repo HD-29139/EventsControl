a031411fcacea6cf2398716fe4d07ae71c8c5b24
HD-29139 <santanajefferson594@gmail.com>
2025-05-04 22:26:55 -0300

    same

--- main/Person.cs
+++ main/Person.cs
@@ -2,15 +2,9 @@ namespace control_public_events.main
 {
     public class Person
     {
-        public string Name { get; set; }
-        public string Address { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
         public int Age { get; set; }
 
-        public Person(string name, string address, int age)
-        {
-            this.Name = name;
-            this.Address = address;
-            this.Age = age;
-        }
     }
 }
