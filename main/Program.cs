fc19df33181a096d78623c3a92725c76b373a4a9
HD-29139 <santanajefferson594@gmail.com>
2025-05-04 22:27:57 -0300

    person register and list events

--- main/Program.cs
+++ main/Program.cs
@@ -23,10 +23,10 @@ class Program
                     EventRegister();
                     break;
                 case "2":
-                    // PersonRegister();
+                    PersonRegister();
                     break;
                 case "3":
-                    // ListEvents();
+                    ListEvents();
                     break;
                 case "4":
                     return;
@@ -37,7 +37,7 @@ class Program
             }
         }
 
-                
+
         static void EventRegister()
         {
             Event newEvent = new Event();
@@ -64,8 +64,61 @@ class Program
             events.Add(newEvent);
             Console.WriteLine("Event registered successfully!");
 
-           
+
         }
-        
+
+        static void PersonRegister()
+        {
+            if (events.Count == 0)
+            {
+                Console.WriteLine("No events registered.");
+                return;
+            }
+
+            Console.WriteLine("Available events:");
+            for (int i = 0; i < events.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}. {events[i].Name}");
+                Console.WriteLine($"{events[i].Participants.Count}|20 Peoples");
+            }
+
+            Console.WriteLine("Choose the event by number: ");
+            int index = Convert.ToInt32(Console.ReadLine()) - 1;
+            if (index < 0 || index >= events.Count)
+            {
+                Console.WriteLine($"Invalid Event{index}");
+                return;
+            }
+
+            Person newPerson = new Person();
+
+            Console.WriteLine("Participant Name: ");
+            newPerson.Name = Console.ReadLine();
+
+            Console.WriteLine("Participant Address: ");
+            newPerson.Address = Console.ReadLine();
+
+            Console.WriteLine("Participant Age: ");
+            newPerson.Age = Convert.ToInt32(Console.ReadLine());
+
+            if (events[index].AddPerson(newPerson))
+            {
+                Console.WriteLine($"Person registered for the event{events[index].Name}");
+            }
+        }
+
+        static void ListEvents()
+        {
+            foreach (var newEvent in events)
+            {
+                Console.WriteLine($"Event Name: {newEvent.Name} | Event Local: {newEvent.Local} | Event Value: R${newEvent.Value}");
+                Console.WriteLine($"Participants ({newEvent.Participants.Count}20):");
+                foreach (var person in newEvent.Participants)
+                {
+                    Console.WriteLine($"- {person.Name} {person.Age} years Address: {person.Address}");
+                }
+            }
+        }
+
     }
 }
